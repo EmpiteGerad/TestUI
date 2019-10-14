831372543a38558218c1df52c0ab139c832fc9a8
Gerad Niroshan <gerad@empite.com>
2019-10-14 18:39:34 +0530

    3 components added

--- src/TestApp/src/app/app-routing.module.ts
+++ src/TestApp/src/app/app-routing.module.ts
@@ -1,8 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GetPatientsComponent } from './get-patients/get-patients.component';
+import { AddPatientComponent } from './add-patient/add-patient.component';
+import { EditPatientComponent } from './edit-patient/edit-patient.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'patients',
+    component: GetPatientsComponent
+  },
+  {
+    path: 'patient/add',
+    component: AddPatientComponent
+  },
+  {
+    path: 'patient/edit/:id',
+    component: EditPatientComponent
+  }
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/TestApp/src/app/app.module.ts
+++ src/TestApp/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GetPatientsComponent } from './get-patients/get-patients.component';
+import { AddPatientComponent } from './add-patient/add-patient.component';
+import { EditPatientComponent } from './edit-patient/edit-patient.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GetPatientsComponent,
+    AddPatientComponent,
+    EditPatientComponent
   ],
   imports: [
     BrowserModule,
