87b7d7c5abf59781818b23b73caab0047195f3c7
Gerad Niroshan <gerad@empite.com>
2019-10-15 19:10:23 +0530

    Mid commit

--- src/TestApp/src/app/app.module.ts
+++ src/TestApp/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { GetPatientsComponent } from './get-patients/get-patients.component';
 import { AddPatientComponent } from './add-patient/add-patient.component';
 import { EditPatientComponent } from './edit-patient/edit-patient.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
